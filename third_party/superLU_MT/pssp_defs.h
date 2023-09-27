80f2fa590d2938f87940feab68262c545a6996ca
David P. Chassin <dchassin@slac.stanford.edu>
2023-09-26 19:32:35 -0700

    Fix superlu compile warnings (#17)
    Signed-off-by: David P. Chassin <dchassin@slac.stanford.edu>
    Signed-off-by: David P. Chassin <david.chassin@me.com>

--- third_party/superLU_MT/pssp_defs.h
+++ third_party/superLU_MT/pssp_defs.h
@@ -264,7 +264,7 @@ extern void pxgstrf_scheduler (const int, const int, const int *,
 			       int *, int *, pxgstrf_shared_t *);
 extern int  sParallelInit (int, pxgstrf_relax_t *, superlumt_options_t *,
 			  pxgstrf_shared_t *);
-extern int  ParallelFinalize ();
+extern int  ParallelFinalize (pxgstrf_shared_t *);
 extern int  queue_init (queue_t *, int);
 extern int  queue_destroy (queue_t *);
 extern int  EnqueueRelaxSnode (queue_t *, int, pxgstrf_relax_t *,
@@ -433,7 +433,7 @@ extern int  *intMalloc (int);
 extern int  *intCalloc (int);
 extern float *floatMalloc(int);
 extern float *floatCalloc(int);
-extern int  memory_usage ();
+extern int  memory_usage (void);
 extern int  superlu_sQuerySpace (int, SuperMatrix *, SuperMatrix *, int, 
 				 superlu_memusage_t *);
 extern int  Glu_alloc (const int, const int, const int, const MemType,
@@ -442,9 +442,9 @@ extern int  Glu_alloc (const int, const int, const int, const MemType,
 /* -------------------
    Auxiliary routines
    -------------------*/
-extern double  SuperLU_timer_();
+extern double  SuperLU_timer_(void);
 extern int     sp_ienv(int);
-extern double  slamch_();
+extern double  slamch_(char *);
 extern int     lsame_(char *, char *);
 extern int     xerbla_(char *, int *);
 extern void    superlu_abort_and_exit(char *);
