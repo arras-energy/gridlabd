40d6b3a6ce29a9d3cdf4e035a2e1de3dc40708f7
David P. Chassin <david.chassin@me.com>
2023-11-19 16:57:48 -0800

    Update nsrdb_weather.py
    Signed-off-by: David P. Chassin <dchassin@slac.stanford.edu>

--- tools/nsrdb_weather.py
+++ tools/nsrdb_weather.py
@@ -522,7 +522,7 @@ if __name__ == "__main__":
     csv = None
 
     if len(sys.argv) == 1:
-        syntax(1)
+        os.system(f"marimo run {os.environ['GLD_ETC']}/gridlabd_weather_ui.py")
     for arg in sys.argv[1:]:
         args = arg.split("=")
         if type(args) is list and len(args) > 1:
