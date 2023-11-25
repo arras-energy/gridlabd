b18c6263618407618a2022dd928a1ae78e931e3c
David P. Chassin <david.chassin@me.com>
2023-11-25 07:10:08 -0800

    Add support for saving to stdout using .ext
    Signed-off-by: David P. Chassin <dchassin@slac.stanford.edu>

--- source/save.cpp
+++ source/save.cpp
@@ -37,7 +37,7 @@ int saveall(const char *filename)
 	/* identify output format */
 	if (ext==NULL)
 	{	/* no extension given */
-		if (filename[0]=='-') /* stdout */
+		if (filename[0]=='.' || filename[0]=='-') /* stdout */
 		{
 			ext=filename+1; /* format is specified after - */
 		}
