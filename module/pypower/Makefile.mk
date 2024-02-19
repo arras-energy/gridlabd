bec797bf78aceea13c35bff310db22f8ef809c77
David P. Chassin <david.chassin@me.com>
2024-02-19 11:09:02 -0800

    Fix compile errors
    Signed-off-by: David P. Chassin <dchassin@slac.stanford.edu>

--- module/pypower/Makefile.mk
+++ module/pypower/Makefile.mk
@@ -3,17 +3,17 @@
 
 pkglib_LTLIBRARIES += module/pypower/pypower.la
 
-module_assert_assert_la_CPPFLAGS =
-module_assert_assert_la_CPPFLAGS += $(AM_CPPFLAGS)
+module_bus_bus_la_CPPFLAGS =
+module_bus_bus_la_CPPFLAGS += $(AM_CPPFLAGS)
 
-module_assert_assert_la_LDFLAGS =
-module_assert_assert_la_LDFLAGS += $(AM_LDFLAGS)
+module_bus_bus_la_LDFLAGS =
+module_bus_bus_la_LDFLAGS += $(AM_LDFLAGS)
 
-module_assert_assert_la_LIBADD = 
+module_bus_bus_la_LIBADD = 
 
-module_assert_assert_la_SOURCES =
-module_assert_assert_la_SOURCES += module/assert/main.cpp module/assert/pypower.h
-module_assert_assert_la_SOURCES += module/assert/bus.cpp module/assert/bus.h
-module_assert_assert_la_SOURCES += module/assert/branch.cpp module/assert/branch.h
-module_assert_assert_la_SOURCES += module/assert/gen.cpp module/assert/gen.h
-module_assert_assert_la_SOURCES += module/assert/gencost.cpp module/assert/gencost.h
+module_bus_bus_la_SOURCES =
+module_bus_bus_la_SOURCES += module/bus/main.cpp module/bus/pypower.h
+module_bus_bus_la_SOURCES += module/bus/bus.cpp module/bus/bus.h
+# module_bus_bus_la_SOURCES += module/bus/branch.cpp module/bus/branch.h
+# module_bus_bus_la_SOURCES += module/bus/gen.cpp module/bus/gen.h
+# module_bus_bus_la_SOURCES += module/bus/gencost.cpp module/bus/gencost.h
