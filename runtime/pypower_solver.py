038aa9256c2ab4677e04d639b195c49069a6999a
David P. Chassin <david.chassin@me.com>
2024-02-19 21:59:30 -0800

    Added solver
    Signed-off-by: David P. Chassin <dchassin@slac.stanford.edu>

--- runtime/pypower_solver.py
+++ runtime/pypower_solver.py
@@ -1,11 +1,25 @@
 import os, sys
-import pypower
+from pypower import runpf
+from numpy import array
 
 with_opf = False
 
 def solver(pf_case):
 
-    print(f"solver(pf_case={pf_case})",file=sys.stderr)
-    
+    data = dict(version=pf_case['version'],baseMVA=pf_case['baseMVA'])
+
+    # copy from model
+    for name in ['bus','branch','gen']:
+        data[name] = array(pf_case[name])
+
+    # TODO: call solver
+    runpf(data)
+
+    # copy back to model
+    for name in ['bus','branch','gen']:
+        pf_case[name] = data[name].tolist()
+
+    print(f"solver(pf_case={pf_case})",file=sys.stderr,flush=True)
+
     return True
 
