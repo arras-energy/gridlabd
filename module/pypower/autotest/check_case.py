0ee9c1d27530d0d5a0e5831bcb5fe1bff9c50a8f
David P. Chassin <david.chassin@me.com>
2024-02-23 09:03:22 -0800

    Create check_case.py

---
+++ module/pypower/autotest/check_case.py
@@ -0,0 +1,39 @@
+import sys, json
+
+VERBOSE = False
+
+if '-v' in sys.argv or '--verbose' in sys.argv:
+    VERBOSE = True
+    if '-v' in sys.argv:
+        sys.argv.remove('-v')
+    if '--verbose' in sys.argv:
+        sys.argv.remove('--verbose')
+
+if len(sys.argv) < 4:
+    print("Syntax: check_case [-v|--verbose] FILE1.json FILE2.json NAME/PROP [...]")
+    exit(1)
+
+with open(sys.argv[1],'r') as fh:
+    ref = json.load(fh)
+
+with open(sys.argv[2],'r') as fh:
+    act = json.load(fh)
+
+assert(ref['application'] == 'gridlabd')
+assert(act['application'] == 'gridlabd')
+
+count = 0
+for spec in sys.argv[3:]:
+    name,prop = spec.split('/')
+    if ref['objects'][name][prop] != act['objects'][name][prop]:
+        if VERBOSE:
+            print(f"ERROR: {spec} differs --> {ref['objects'][name][prop]} != {act['objects'][name][prop]}", file=sys.stderr)
+        count += 1
+    else:
+        if VERBOSE:
+            print(f"{spec} ok --> {ref['objects'][name][prop]} == {act['objects'][name][prop]}", file=sys.stderr)
+
+if VERBOSE:
+    print(f"{count} differences found")
+
+exit(2 if count>0 else 0)
