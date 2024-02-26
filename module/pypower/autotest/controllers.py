f0c0d891f4a3bc2f873c2131ed2f4f9d9ff34636
David P. Chassin <david.chassin@me.com>
2024-02-25 21:14:31 -0800

    Update controllers.py

--- module/pypower/autotest/controllers.py
+++ module/pypower/autotest/controllers.py
@@ -5,9 +5,9 @@ def on_init():
     return True
 
 def load_control(obj,**kwargs):
-    print(obj,": load control update",kwargs,file=sys.stderr)
+    # print(obj,": load control update",kwargs,file=sys.stderr)
     return dict(t=kwargs['t']+3600, S=(15+2j))
 
 def powerplant_control(obj,**kwargs):
-    print(obj,": powerplant control update",kwargs,file=sys.stderr)
+    # print(obj,": powerplant control update",kwargs,file=sys.stderr)
     return dict(t=kwargs['t']+3600, S="15+2j kW")
