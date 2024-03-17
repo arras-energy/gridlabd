fa25e253e6160c51a07112dd8f979f18655de6f4
David P. Chassin <dchassin@slac.stanford.edu>
2024-03-17 08:17:43 -0700

    Update transformer model

--- module/pypower/transformer.cpp
+++ module/pypower/transformer.cpp
@@ -28,24 +28,25 @@ transformer::transformer(MODULE *module)
 				PT_REQUIRED,
 				PT_DESCRIPTION, "transformer impedance (Ohm)",
 
-			PT_enumeration, "status", get_status_offset(),
-				PT_DEFAULT, "IN",
-				PT_KEYWORD, "IN", (enumeration)TS_IN,
-				PT_KEYWORD, "OUT", (enumeration)TS_OUT,
-				PT_DESCRIPTION, "transformer status (IN or OUT)",
-
-			PT_double, "phase_shift[deg]", get_phase_shift_offset(),
-				PT_DESCRIPTION, "transformer phase shift (deg) - use 30 for DY or YD transformers",
+			PT_double, "susceptance[S]", get_susceptance_offset(),
+				PT_DEFAULT, "0 S",
 
 			PT_double, "rated_power[MVA]", get_rated_power_offset(),
 				PT_REQUIRED,
 				PT_DESCRIPTION, "transformer power rating (MVA)",
 
-			PT_double, "primary_voltage[kV]", get_primary_voltage_offset(),
-				PT_DESCRIPTION, "primary winding nominal voltage (kV)",
+			PT_double, "tap_ratio[pu]", get_tap_ratio_offset(),
+				PT_DEFAULT, "1 pu",
+				PT_DESCRIPTION, "off-normal turns ratio",
 
-			PT_double, "secondary_voltage[kV]", get_secondary_voltage_offset(),
-				PT_DESCRIPTION, "secondary winding nominal_voltage (kV)",
+			PT_double, "phase_shift[deg]", get_phase_shift_offset(),
+				PT_DESCRIPTION, "transformer phase shift (deg) - use 30 for DY or YD transformers",
+
+			PT_enumeration, "status", get_status_offset(),
+				PT_DEFAULT, "IN",
+				PT_KEYWORD, "IN", (enumeration)TS_IN,
+				PT_KEYWORD, "OUT", (enumeration)TS_OUT,
+				PT_DESCRIPTION, "transformer status (IN or OUT)",
 
 			NULL) < 1 )
 		{
@@ -61,8 +62,39 @@ int transformer::create(void)
 
 int transformer::init(OBJECT *parent_hdr)
 {
+
+	// check impedance
+	if ( impedance.Re() <= 0 && impedance.Im() == 0 )
+	{
+		error("transformer impedance must be positive");
+		return 0;
+	}
+
+	// check susceptance
+	if ( susceptance.Re() < 0 )
+	{
+		error("transformer susceptance cannot be negative");
+		return 0;
+	}
+
+	// check tap ratio
+	if ( get_tap_ratio() <= 0 )
+	{
+		error("tap ratio must be positive");
+	}
+	else if ( get_tap_ratio() < 0.8 || get_tap_ratio() > 1.2 )
+	{
+		warning("tap ratio outside reasonable range of 0.8 to 1.2");
+	}
+
+	// check angle
+	if ( fabs(get_phase_shift()) > 360 )
+	{
+		warning("phase shift value %.4lg seems unlikely to be valid",get_phase_shift());
+	}
+
 	branch *parent = (branch*)get_parent();
-	if ( parent ) 
+	if ( parent )
 	{
 		if ( parent->isa("branch","pypower") )
 		{
@@ -79,53 +111,17 @@ int transformer::init(OBJECT *parent_hdr)
 			error("parent '%s' is not a pypower branch",get_parent()->get_name());
 			return 0;
 		}
+		parent->set_ratio(get_tap_ratio());
+		parent->set_angle(get_phase_shift());
+		parent->set_r(get_impedance().Re());
+		parent->set_x(get_impedance().Im());
+		parent->set_b(get_susceptance());
+		parent->set_rateA(get_rated_power());
+		parent->set_status(get_status() == TS_IN ? branch::BS_IN : branch::BS_OUT);
 	}
 	else
 	{
 		warning("transformer without parent branch does nothing");
-	}
-
-	// check impedance
-	if ( impedance.Re() == 0 && impedance.Im() == 0 )
-	{
-		error("transformer impedance must be positive");
-		return 0;
-	}
-
-	// check/get nominal voltages
-	if ( get_primary_voltage() == 0 )
-	{
-		// TODO copy from branch fbus
-		error("primary winding nominal voltage is not specified");
-	}
-	else
-	{
-		// TODO compare to branch fbus
-	}
-	if ( get_secondary_voltage() == 0 )
-	{
-		// TODO copy from branch tbus
-		error("secondary winding nominal voltage is not specified");
-	}
-	else
-	{
-		// TODO compare to branch tbus
-	}
-
-	// check angle
-	if ( fabs(get_phase_shift()) > 360 )
-	{
-		warning("phase shift value %.4lg seems unlikely to be valid",get_phase_shift());
-	}
-
-	if ( parent )
-	{
-		parent->set_ratio(1.0);
-		parent->set_angle(get_phase_shift());
-		parent->set_r(get_impedance().Re());
-		parent->set_x(get_impedance().Im());
-		parent->set_b(0.0);
-		parent->set_rateA(get_rated_power());
 	}
 
 	return 1; // return 1 on success, 0 on failure, 2 on retry later
@@ -136,16 +132,8 @@ TIMESTAMP transformer::precommit(TIMESTAMP t0)
 	if ( get_parent() != NULL )
 	{
 		branch *parent = (branch*)get_parent();
-
-		if ( get_status() == TS_IN )
-		{
-			// copy status/impedance/admittance values to branch
-			parent->set_status(branch::BS_IN);
-		}
-		else
-		{
-			parent->set_status(branch::BS_OUT);
-		}
+		parent->set_ratio(get_tap_ratio());
+		parent->set_status(get_status() == TS_IN ? branch::BS_IN : branch::BS_OUT);
 	}
 	return TS_NEVER;
 }
