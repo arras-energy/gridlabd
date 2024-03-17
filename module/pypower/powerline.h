92c89b24369935b5563028e64e5b047c0fd40171
David P. Chassin <dchassin@slac.stanford.edu>
2024-03-17 16:02:13 -0700

    Add support for PSS/E converter (#182)
    Signed-off-by: David P. Chassin <dchassin@slac.stanford.edu>
    Signed-off-by: Alyona Teyber <Ivanova.alyona5@gmail.com>
    Signed-off-by: David P. Chassin <david.chassin@me.com>
    Co-authored-by: Alyona Teyber <Ivanova.alyona5@gmail.com>
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>

--- module/pypower/powerline.h
+++ module/pypower/powerline.h
@@ -13,10 +13,11 @@ public:
 	// published properties
 	GL_ATOMIC(double,length);
 	GL_ATOMIC(complex,impedance);
+	GL_ATOMIC(double,susceptance);
 	GL_ATOMIC(double,rating);
 	typedef enum {PLS_OUT=0,PLS_IN=1} POWERLINESTATUS;
 	GL_ATOMIC(enumeration,status);
-	typedef enum {PLC_SERIES=1,PLC_PARALLEL=2} POWERLINECOMPOSITION;
+	typedef enum {PLC_NONE=0,PLC_SERIES=1,PLC_PARALLEL=2} POWERLINECOMPOSITION;
 	GL_ATOMIC(enumeration,composition);
 
 public:
@@ -25,6 +26,7 @@ public:
 	GL_ATOMIC(double,rateA);
 	GL_ATOMIC(complex,Z);
 	GL_ATOMIC(complex,Y);
+	GL_ATOMIC(double,b);
 
 public:
 	bool parent_is_branch;
@@ -34,7 +36,9 @@ public:
 	powerline(MODULE *module);
 	int create(void);
 	int init(OBJECT *parent);
-	TIMESTAMP precommit(TIMESTAMP t1);
+	TIMESTAMP presync(TIMESTAMP t1);
+	TIMESTAMP sync(TIMESTAMP t1);
+	TIMESTAMP postsync(TIMESTAMP t1) { return TS_NEVER; };
 
 public:
 	// internal properties
