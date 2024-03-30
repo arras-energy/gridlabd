8f0759490ffd1440fc348371650220242721e15f
David P. Chassin <dchassin@slac.stanford.edu>
2024-03-30 08:52:07 -0700

    Add system properties update
    Signed-off-by: David P. Chassin <dchassin@slac.stanford.edu>

--- module/behavior/system.cpp
+++ module/behavior/system.cpp
@@ -8,10 +8,12 @@ EXPORT_INIT(system);
 EXPORT_PRECOMMIT(system);
 EXPORT_METHOD(system,u);
 EXPORT_METHOD(system,p);
-EXPORT_METHOD(system,Z);
+EXPORT_METHOD(system,device);
 
 CLASS *system::oclass = NULL;
 class system *system::defaults = NULL;
+
+double system::resolution = 1e-9;
 
 system::system(MODULE *module)
 {
@@ -26,6 +28,9 @@ system::system(MODULE *module)
 
 		defaults = this;
 		if (gl_publish_variable(oclass,
+
+			PT_double, "sigma", get_sigma_offset(),
+				PT_DESCRIPTION, "System activity",
 
 			PT_double, "tau", get_tau_offset(),
 				PT_DESCRIPTION, "System activity",
@@ -42,18 +47,36 @@ system::system(MODULE *module)
 			PT_method, "p", get_p_offset(),
 				PT_DESCRIPTION, "State probability",
 
-			PT_method, "Z", get_Z_offset(),
+			PT_double, "Z", get_Z_offset(),
 				PT_DESCRIPTION, "State partition function",
+
+			PT_double, "Navg", get_Navg_offset(),
+				PT_DESCRIPTION, "Average number of devices in system",
+
+			PT_double, "Uavg", get_Uavg_offset(),
+				PT_DESCRIPTION, "Average device value in system",
+
+			PT_method, "device", get_device_offset(),
+				PT_DESCRIPTION, "Property of device connected to this system",
 
 			NULL)<1)
 		{
 				throw "unable to publish system properties";
 		}
+
+	    gl_global_create("behavior::system_resolution",
+	        PT_double, &resolution, 
+	        PT_DESCRIPTION, "Resolution of system properties",
+	        NULL);
 	}
 }
 
 int system::create(void) 
 {
+	n_values = 0;
+	values = NULL;
+	probs = NULL;
+
 	n_points = 0;
 	max_points = 1024;
 	point_list = (gld_property**)malloc(sizeof(gld_property*)*max_points);
@@ -65,11 +88,90 @@ int system::create(void)
 
 int system::init(OBJECT *parent)
 {
+	if ( n_values < 2 )
+	{
+		error("there must be at least 2 state values u specified");
+		return 0;
+	}
+	update();
 	return 1; // return 1 on success, 0 on failure, 2 on retry later
+}
+
+void system::update(void)
+{
+	Z = 0.0;
+	Navg = 0.0;
+	Uavg = 0.0;
+	double u_min = 0;
+	bool zero_tau = ( fabs(tau) <= resolution );
+	if ( ! zero_tau )
+	{
+		for ( int n = 0 ; n < n_values ; n++ )
+		{
+			double x = round(exp((N*mu-values[n])/tau)/resolution)*resolution;
+			if ( isfinite(x) && ! isnan(x) && x >= resolution && x < 1/resolution )
+			{
+				probs[n] = x;
+				Z += x;
+			}
+			else
+			{
+				probs[n] = 0.0;
+			}
+		}
+		if ( fabs(Z) < resolution )
+		{
+			zero_tau = true;
+			Z = 0;
+		}
+	}
+	for ( int n = 0 ; n < n_values ; n++ )
+	{
+		if ( zero_tau && tau >= 0 )
+		{
+			u_min = ( n == 0 || values[n] < u_min ? values[n] : u_min );
+			probs[n] = 0;
+		}
+		else if ( zero_tau && tau < 0 )
+		{
+			u_min = ( n == 0 || values[n] > u_min ? values[n] : u_min );
+			probs[n] = 0;
+		}
+		else
+		{
+			double x = probs[n];
+			Navg += N*x;
+			Uavg += values[n]*x;
+		}
+	}
+	if ( zero_tau )
+	{
+		for ( int n = 0 ; n < n_values ; n++ )
+		{
+			if ( values[n] == u_min )
+			{
+				probs[n] = 1;
+				Navg += N;
+				Uavg += values[n];
+				Z++;
+			}
+		}
+	}
+	for ( int n = 0 ; n < n_values ; n++ )
+	{
+		probs[n] = round(probs[n]/Z/resolution)*resolution;
+	}
+	Navg = round(Navg/Z/resolution)*resolution;
+	Uavg = round(Uavg/Z/resolution)*resolution;
+	if ( zero_tau )
+	{
+		Z = 0;
+	}
 }
 
 TIMESTAMP system::precommit(TIMESTAMP t0)
 {
+	update();
 	return TS_NEVER;
 }
 
@@ -78,18 +180,44 @@ int system::u(char *buffer, size_t len)
 	if ( buffer == NULL )
 	{
 		// get size of output buffer needed
-		return strlen(point_names);
+		len = 0;
+		for ( int n = 0 ; n < n_values ; n++ )
+		{
+			int sz = snprintf(NULL,0,"%g",values[n]);
+			if ( sz < 0 )
+			{
+				return -1;
+			}
+			len += sz + 1;
+		}
+		return len;
 	}
 	else if ( len == 0 )
 	{
 		// read data from buffer
-		int len = 0;
-		return len;
+		char *next=NULL, *last=NULL;
+		while ( (next=strtok_r(next?NULL:buffer,",",&last)) != NULL )
+		{
+			values = (double*)realloc(values,(n_values+1)*(sizeof(*values)));
+			probs = (double*)realloc(probs,(n_values+1)*(sizeof(*values)));
+			if ( values == NULL || probs == NULL )
+			{
+				exception("memory allocation error reallocating values buffer");
+			}
+			values[n_values] = atof(next);
+			n_values++;
+		}
+		return strlen(buffer);
 	}
 	else
 	{
-		// write to buffer
-		return 0;
+		// write data to buffer
+		size_t sz = 0;
+		for ( int n = 0 ; n < n_values && sz < len ; n++ )
+		{
+			sz += snprintf(buffer+sz,len-sz+1,"%s%g",sz>0?",":"",values[n]);
+		}
+		return sz;
 	}
 }
 
@@ -98,37 +226,87 @@ int system::p(char *buffer, size_t len)
 	if ( buffer == NULL )
 	{
 		// get size of output buffer needed
-		return strlen(point_names);
+		len = 0;
+		for ( int n = 0 ; n < n_values ; n++ )
+		{
+			int sz = snprintf(NULL,0,"%g",probs[n]);
+			if ( sz < 0 )
+			{
+				return -1;
+			}
+			len += sz + 1;
+		}
+		return len;
 	}
 	else if ( len == 0 )
 	{
 		// read data from buffer
-		int len = 0;
-		return len;
+		char *next=NULL, *last=NULL;
+		int n = 0;
+		while ( (next=strtok_r(next?NULL:buffer,",",&last)) != NULL )
+		{
+			if ( n < n_values )
+			{
+				probs[n++] = atof(next);
+			}
+			else
+			{
+				error("too many probabilities for %d states",n);
+				return -1;
+			}
+		}
+		return strlen(buffer);
 	}
 	else
 	{
-		// write to buffer
-		return 0;
+		// write data to buffer
+		size_t sz = 0;
+		for ( int n = 0 ; n < n_values && sz < len ; n++ )
+		{
+			sz += snprintf(buffer+sz,len-sz+1,"%s%g",sz>0?",":"",probs[n]);
+		}
+		return sz;
 	}
 }
 
-int system::Z(char *buffer, size_t len)
+int system::device(char *buffer, size_t len)
 {
 	if ( buffer == NULL )
 	{
-		// get size of output buffer needed
-		return strlen(point_names);
+		return point_names ? strlen(point_names) : 0;
 	}
 	else if ( len == 0 )
 	{
 		// read data from buffer
-		int len = 0;
+		if ( n_points == max_points )
+		{
+			max_points *= 2;
+			point_list = (gld_property**)realloc(point_list,max_points);
+			name_list = (char**)realloc(name_list,max_points);
+		}
+		point_list[n_points] = new gld_property(buffer);
+		name_list[n_points] = strdup(buffer);
+		if ( point_list[n_points] == NULL || name_list[n_points] == NULL )
+		{
+			error("memory allocation failure");
+			return 0;
+		}
+		if ( point_list[n_points]->get_type() != PT_double )
+		{
+			error("target point is not a double-type value");
+			return 0;
+		}
+		n_points++;
+
+		// copy name to list of names
+		int pos = strlen(point_names);
+		len = strlen(buffer);
+		point_names = (char*)realloc(point_names,pos+len+3);
+		snprintf(point_names+pos,len+2,"%s%s",pos>0?",":"",buffer);
 		return len;
 	}
 	else
 	{
-		// write to buffer
-		return 0;
+		return snprintf(buffer,len-1,"%s",point_names);
 	}
 }
--- module/behavior/system.h
+++ module/behavior/system.h
@@ -9,26 +9,45 @@
 DECL_METHOD(system,u);
 DECL_METHOD(system,p);
 DECL_METHOD(system,Z);
+DECL_METHOD(system,device);
 
 class system : public gld_object 
 {
 
 public:
+	static double resolution;
+
+public:
+
 	// published properties
+	GL_ATOMIC(double,sigma);
 	GL_ATOMIC(double,tau);
 	GL_ATOMIC(double,mu);
 	GL_ATOMIC(double,N);
-	GL_METHOD(system,Z);
+	GL_ATOMIC(double,Z);
+	GL_ATOMIC(double,Navg);
+	GL_ATOMIC(double,Uavg);
 	GL_METHOD(system,u);
 	GL_METHOD(system,p);
+	GL_METHOD(system,device);
 
 private:
+
+	// internal variables
+	double *values;
+	double *probs;
+	int n_values;
 
 	gld_property **point_list;
 	char **name_list;
 	size_t n_points;
 	size_t max_points;
 	char *point_names;
+
+private:
+
+	// internal functions
+	void update(void);
 
 public:
 
@@ -39,6 +58,7 @@ public:
 	TIMESTAMP precommit(TIMESTAMP t0);
 
 public:
+
 	// internal properties
 	static CLASS *oclass;
 	static system *defaults;
