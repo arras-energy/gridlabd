1ec02399df5d5ac0b3b1eb0ba76291c525b6dfc2
David P. Chassin <david.chassin@me.com>
2024-06-04 20:48:28 -0700

    Added source to fgetln so it compiles on linux system that don't have it built-in
    Signed-off-by: David P. Chassin <david.chassin@me.com>

--- module/tape/multiplayer.cpp
+++ module/tape/multiplayer.cpp
@@ -258,6 +258,62 @@ int multiplayer::file(char *buffer, size_t len)
 	return -1;
 }
 
+// #ifndef MACOSX
+char *fgetln(FILE *fp, size_t *len)
+{
+	static char *buf = NULL;
+	static size_t bufsiz = 0;
+	char *ptr;
+
+	if ( buf == NULL ) 
+	{
+		bufsiz = BUFSIZ;
+		if ( (buf = (char*)malloc(bufsiz)) == NULL )
+		{
+			return NULL;
+		}
+	}
+
+	if ( fgets(buf, bufsiz, fp) == NULL)
+	{
+		return NULL;
+	}
+
+	*len = 0;
+	while ( (ptr = strchr(&buf[*len], '\n')) == NULL) 
+	{
+		size_t nbufsiz = bufsiz + BUFSIZ;
+		char *nbuf = (char*)realloc(buf, nbufsiz);
+
+		if ( nbuf == NULL ) 
+		{
+			int oerrno = errno;
+			free(buf);
+			errno = oerrno;
+			buf = NULL;
+			return NULL;
+		} 
+		else
+		{
+			buf = nbuf;
+		}
+
+		if ( fgets(&buf[bufsiz], BUFSIZ, fp) == NULL ) 
+		{
+			buf[bufsiz] = '\0';
+			*len = strlen(buf);
+			return buf;
+		}
+
+		*len = bufsiz;
+		bufsiz = nbufsiz;
+	}
+
+	*len = (ptr - buf) + 1;
+	return buf;
+}
+// #endif
+
 bool multiplayer::load(void)
 {
 	fseek(fp,0,SEEK_SET);
