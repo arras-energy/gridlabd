e4d42a0f99c9ea10c9eea0433aa583b75fea23ac
David P. Chassin <david.chassin@me.com>
2024-09-08 18:35:47 +0200

    Release 4.3.10 (#197)
    Signed-off-by: David P. Chassin <dchassin@slac.stanford.edu>
    Signed-off-by: Alyona Teyber <Ivanova.alyona5@gmail.com>
    Signed-off-by: David P. Chassin <david.chassin@me.com>
    Signed-off-by: Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
    Co-authored-by: David P. Chassin <dchassin@slac.stanford.edu>
    Co-authored-by: Alyona Teyber <Ivanova.alyona5@gmail.com>
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
    Signed-off-by: David P. Chassin <david.chassin@me.com>

--- module/pypower/autotest/controllers.py
+++ module/pypower/autotest/controllers.py
@@ -12,26 +12,35 @@ def on_sync(data):
 def load_control(obj,**kwargs):
     # print(f"load_control({obj})",kwargs,file=sys.stderr)
     if kwargs['t']%3600 < 1800 and kwargs['P'] != 0: # turn off load in first half-hour
-        return dict(P=0)
+        return dict(t=(int(kwargs['t']/1800)+1)*1800,P=0)
     elif kwargs['t']%3600 >= 1800 and kwargs['P'] == 0: # turn on load in second half-hour
-        return dict(P=10)
+        return dict(t=(int(kwargs['t']/1800)+1)*1800,P=10)
     else: # no change -- advance to next 1/2 hour when a change is anticipated
         return dict(t=(int(kwargs['t']/1800)+1)*1800)
 
 def powerplant_control(obj,**kwargs):
     # print(f"powerplant_control({obj})",kwargs,file=sys.stderr)
-    if kwargs['t']%3600 < 1800 and kwargs['S'].real != 0: # turn off plant in first half-hour
-        return dict(S=(0j))
-    elif kwargs['t']%3600 >= 1800 and kwargs['S'].real == 0: # turn on plant in second half-hour
-        return dict(S=(10+0j))
+    if kwargs['t']%3600 < 1800: # turn off plant in first half-hour
+        return dict(t=(int(kwargs['t']/1800)+1)*1800,S=(0j))
+    elif kwargs['t']%3600 >= 1800: # turn on plant in second half-hour
+        return dict(t=(int(kwargs['t']/1800)+1)*1800,S=(10+0j))
+    else: # no change -- advance to next 1/2 hour when a change is anticipated
+        return dict(t=(int(kwargs['t']/1800)+1)*1800)
+
+def storage_control(obj,**kwargs):
+    # print(f"powerplant_control({obj})",kwargs,file=sys.stderr)
+    if kwargs['t']%3600 < 1800: # discharge in first half-hour
+        return dict(t=(int(kwargs['t']/1800)+1)*1800,S=(-0.001+0j))
+    elif kwargs['t']%3600 >= 1800: # charge in second half-hour
+        return dict(t=(int(kwargs['t']/1800)+1)*1800,S=(0.01+0j))
     else: # no change -- advance to next 1/2 hour when a change is anticipated
         return dict(t=(int(kwargs['t']/1800)+1)*1800)
 
 def relay_control(obj,**kwargs):
     # print(f"relay_control({obj})",kwargs,file=sys.stderr)
     if kwargs['t']%3600 < 1800 and kwargs['status'] != 0: # open the relay in first half-hour
-        return dict(status=0)
+        return dict(t=(int(kwargs['t']/1800)+1)*1800,status=0)
     elif kwargs['t']%3600 >= 1800 and kwargs['status'] == 0: # close the relay in second half-hour
-        return dict(status=1)
+        return dict(t=(int(kwargs['t']/1800)+1)*1800,status=1)
     else: # no change -- advance to next 1/2 hour when a change is anticipated
         return dict(t=(int(kwargs['t']/1800)+1)*1800)
--- module/pypower/powerplant.cpp
+++ module/pypower/powerplant.cpp
@@ -5,7 +5,9 @@
 
 EXPORT_CREATE(powerplant);
 EXPORT_INIT(powerplant);
+EXPORT_PRECOMMIT(powerplant);
 EXPORT_SYNC(powerplant);
+EXPORT_COMMIT(powerplant);
 
 CLASS *powerplant::oclass = NULL;
 powerplant *powerplant::defaults = NULL;
@@ -46,36 +48,36 @@ powerplant::powerplant(MODULE *module)
 				PT_DESCRIPTION, "Generator plant code number",
 			
 			PT_set, "generator", get_generator_offset(), 
-			    PT_KEYWORD, "UNKNOWN", (set)0x00000001, 
-			    PT_KEYWORD, "HT", (set)0x00000002, // hydro turbine
-			    PT_KEYWORD, "ST", (set)0x00000004, // steam turbine
-			    PT_KEYWORD, "AT", (set)0x00000008, // compressed air turbine
-			    PT_KEYWORD, "IC", (set)0x00000010, // internal combustion
-			    PT_KEYWORD, "FW", (set)0x00000020, // flywheel
-			    PT_KEYWORD, "WT", (set)0x00000040, // wind turbine
-			    PT_KEYWORD, "ES", (set)0x00000080, // energy storage inverter
-			    PT_KEYWORD, "CT", (set)0x00000100, // combustion turbine
-			    PT_KEYWORD, "PV", (set)0x00000200, // photovoltaic inverter
-			    PT_KEYWORD, "CC", (set)0x00000400, // combined cycle turbine
+			    PT_KEYWORD, "UNKNOWN", (set)GT_UNKNOWN, 
+			    PT_KEYWORD, "HT", (set)GT_HYDROTURBINE,
+			    PT_KEYWORD, "ST", (set)GT_STEAMTURBINE,
+			    PT_KEYWORD, "AT", (set)GT_COMPRESSEDAIR,
+			    PT_KEYWORD, "IC", (set)GT_INTERNALCOMBUSTION,
+			    PT_KEYWORD, "FW", (set)GT_FLYWHEEL,
+			    PT_KEYWORD, "WT", (set)GT_WINDTURBINE,
+			    PT_KEYWORD, "ES", (set)GT_ENERGYSTORAGE,
+			    PT_KEYWORD, "CT", (set)GT_COMBUSTIONTURBINE,
+			    PT_KEYWORD, "PV", (set)GT_PHOTOVOLTAIC,
+			    PT_KEYWORD, "CC", (set)GT_COMBINEDCYCLE,
 				PT_DESCRIPTION, "Generator type",
 
 			PT_set, "fuel", get_fuel_offset(), 
-			    PT_KEYWORD, "ELEC", (set)0x00000001, 
-			    PT_KEYWORD, "WIND", (set)0x00000002,
-			    PT_KEYWORD, "SUN", (set)0x00000004, 
-			    PT_KEYWORD, "GEO", (set)0x00000008, 
-			    PT_KEYWORD, "COKE", (set)0x00000010, 
-			    PT_KEYWORD, "WASTE", (set)0x00000020, 
-			    PT_KEYWORD, "BIO", (set)0x00000040, 
-			    PT_KEYWORD, "OIL", (set)0x00000080, 
-			    PT_KEYWORD, "UNKNOWN", (set)0x00000100, 
-			    PT_KEYWORD, "WOOD", (set)0x00000200, 
-			    PT_KEYWORD, "OTHER", (set)0x00000400, 
-			    PT_KEYWORD, "GAS", (set)0x00000800, 
-			    PT_KEYWORD, "NUC", (set)0x00001000, 
-			    PT_KEYWORD, "WATER", (set)0x00002000, 
-			    PT_KEYWORD, "COAL", (set)0x00004000, 
-			    PT_KEYWORD, "NG", (set)0x00008000, 
+			    PT_KEYWORD, "ELEC", (set)FT_ELECTRICITY, 
+			    PT_KEYWORD, "WIND", (set)FT_WIND,
+			    PT_KEYWORD, "SUN", (set)FT_SOLAR, 
+			    PT_KEYWORD, "GEO", (set)FT_GEOTHERMAL, 
+			    PT_KEYWORD, "COKE", (set)FT_COKE, 
+			    PT_KEYWORD, "WASTE", (set)FT_WASTE, 
+			    PT_KEYWORD, "BIO", (set)FT_BIOMASS, 
+			    PT_KEYWORD, "OIL", (set)FT_OIL, 
+			    PT_KEYWORD, "UNKNOWN", (set)FT_UNKNOWN, 
+			    PT_KEYWORD, "WOOD", (set)FT_WOOD, 
+			    PT_KEYWORD, "OTHER", (set)FT_OTHER, 
+			    PT_KEYWORD, "GAS", (set)FT_GAS, 
+			    PT_KEYWORD, "NUC", (set)FT_NUCLEAR, 
+			    PT_KEYWORD, "WATER", (set)FT_WATER, 
+			    PT_KEYWORD, "COAL", (set)FT_COAL, 
+			    PT_KEYWORD, "NG", (set)FT_NATURALGAS, 
 				PT_DESCRIPTION, "Generator fuel type",
 
 			PT_enumeration, "status", get_status_offset(),
@@ -101,11 +103,51 @@ powerplant::powerplant(MODULE *module)
 			PT_char256, "substation_2", get_substation_2_offset(),
 				PT_DESCRIPTION, "Substation 2 id",
 
+			PT_double, "storage_capacity[MWh]", get_storage_capacity_offset(),
+				PT_DESCRIPTION, "Energy storage capacity (MWh)",
+
+			PT_double, "charging_capacity[MW]", get_charging_capacity_offset(),
+				PT_DESCRIPTION, "Energy storage charging capacity (MW)",
+
+			PT_double, "storage_efficiency[pu]", get_storage_efficiency_offset(),
+				PT_DEFAULT, "1 pu",
+				PT_DESCRIPTION, "Energy storage round-trip efficiency (pu)",
+
+			PT_double, "state_of_charge[pu]", get_state_of_charge_offset(),
+				PT_DESCRIPTION, "Energy storage state of charge (pu)",
+
 			PT_complex, "S[MVA]", get_S_offset(),
 				PT_DESCRIPTION, "power generation (MVA)",
 
 			PT_char256, "controller", get_controller_offset(),
-				PT_DESCRIPTION,"controller python function name",
+				PT_DESCRIPTION, "controller python function name",
+
+			PT_double, "startup_cost[$/MW]", get_startup_cost_offset(),
+				PT_DESCRIPTION, "generator startup cost ($/MW)",
+
+			PT_double, "shutdown_cost[$/MW]", get_shutdown_cost_offset(),
+				PT_DESCRIPTION, "generator shutdown cost ($/MW)",
+
+			PT_double, "fixed_cost[$/h]", get_fixed_cost_offset(),
+				PT_DESCRIPTION, "generator fixed cost ($/h)",
+
+			PT_double, "variable_cost[$/MWh]", get_variable_cost_offset(),
+				PT_DESCRIPTION, "generator variable cost ($/MWh)",
+
+			PT_double, "scarcity_cost[$/MW^2/h]", get_scarcity_cost_offset(),
+				PT_DESCRIPTION, "generator scarcity cost ($/MW^2h)",
+
+			PT_double, "energy_rate[MWh/unit]", get_energy_rate_offset(),
+				PT_DESCRIPTION, "generator heat rate/fuel efficiency (MWh/unit)",
+
+			PT_double, "total_cost[$]", get_total_cost_offset(),
+				PT_DESCRIPTION, "generator total operating cost ($)",
+
+			PT_double, "emissions_rate[tonne/MWh]", get_energy_rate_offset(),
+				PT_DESCRIPTION, "generator heat rate/fuel efficiency (tonne/MWh)",
+
+			PT_double, "total_emissions[tonne]", get_total_cost_offset(),
+				PT_DESCRIPTION, "generator total operating cost (tonne)",
 
 			NULL) < 1 )
 		{
@@ -119,6 +161,8 @@ int powerplant::create(void)
 	py_controller = NULL;
 	py_args = PyTuple_New(1);
 	py_kwargs = PyDict_New();
+	last_t = 0;
+	last_Sm = 0;
 
 	return 1; // return 1 on success, 0 on failure
 }
@@ -131,6 +175,10 @@ int powerplant::init(OBJECT *parent_hdr)
 		if ( parent->isa("gen","pypower") )
 		{
 			is_dynamic = TRUE;
+			if ( get_storage_capacity() > 0 )
+			{
+				warning("energy storage devices cannot be dynamically dispatchable (parent is a generator)");
+			}
 		}
 		else if ( parent->isa("bus","pypower") )
 		{
@@ -140,6 +188,23 @@ int powerplant::init(OBJECT *parent_hdr)
 		{
 			error("parent '%s' is not a pypower bus or gen object",get_parent()->get_name());
 			return 0;
+		}
+
+		// look for gencost object that corresponds to this generator (if any)
+		for ( OBJECT *obj = gl_object_get_first() ; obj != NULL ; obj = obj->next)
+		{
+			if ( gl_object_isa(obj,"gencost","pypower") )
+			{
+				costobj = OBJECTDATA(obj,gencost);
+				if ( costobj->get_parent() == parent )
+				{
+					break; // got it
+				}
+				else
+				{
+					costobj = NULL; // forget about it!
+				}
+			}
 		}
 	}
 
@@ -196,9 +261,76 @@ int powerplant::init(OBJECT *parent_hdr)
 	PyDict_SetItemString(py_kwargs,"controller",PyUnicode_FromString(get_controller()));
 	PyDict_SetItemString(py_kwargs,"t",PyFloat_FromDouble((double)gl_globalclock));
 
+	// check costs/emissions for units that should have them
+	switch (get_fuel())
+	{
+	case FT_COKE:
+	case FT_BIOMASS:
+	case FT_OIL:
+	case FT_WOOD:
+	case FT_GAS:
+	case FT_NATURALGAS:
+	case FT_COAL:
+		extern bool with_emissions;
+		if ( with_emissions && get_emissions_rate() == 0.0 )
+		{
+			warning("no emissions rate given for a unit that should have a non-zero emissions");
+		}
+	case FT_ELECTRICITY:
+		if ( get_variable_cost() == 0.0 )
+		{
+			warning("no variable cost given for a unit that should have a non-zero fuel cost");
+		}
+	default:
+		break;
+	}
+
 	return 1; // return 1 on success, 0 on failure, 2 on retry later
 }
 
+TIMESTAMP powerplant::precommit(TIMESTAMP t0)
+{
+	if ( last_t > 0 && get_storage_capacity() > 0 )
+	{
+		double Dt = double(t0-last_t)/3600.0;
+		
+		double DE = S.Re();
+		if ( DE > get_charging_capacity() )
+		{
+			DE = get_charging_capacity();
+		}
+		else if ( DE < -get_charging_capacity() )
+		{
+			DE = -get_charging_capacity();
+		}
+
+		double Et = get_storage_capacity()*get_state_of_charge() + DE*Dt*get_storage_efficiency();
+		if ( Et > get_storage_capacity() )
+		{
+			set_state_of_charge(1.0);
+		}
+		else if ( Et < 0 )
+		{
+			set_state_of_charge(0.0);
+		}
+		else
+		{
+			set_state_of_charge(Et/get_storage_capacity());
+		}
+	}
+
+	// post costs
+	if ( costobj != NULL )
+	{
+		costobj->set_startup(get_startup_cost());
+		costobj->set_shutdown(get_shutdown_cost());
+		char buffer[1025];
+		snprintf(buffer,sizeof(buffer)-1,"%lf,%lf,%lf",get_scarcity_cost(),get_variable_cost(),get_fixed_cost());
+		costobj->set_costs(buffer);
+	}
+
+	return TS_NEVER;
+}
 TIMESTAMP powerplant::presync(TIMESTAMP t0)
 {
 	if ( get_status() == 0 )
@@ -311,5 +443,33 @@ TIMESTAMP powerplant::postsync(TIMESTAMP t0)
 {
 	// cannot separate contribution of this powerplant to total gen Pg,Qg
 	exception("invalid postsync event requrest");
+	return TS_NEVER;
+}
+
+TIMESTAMP powerplant::commit(TIMESTAMP t0, TIMESTAMP t1)
+{
+	double dt = (t0 - last_t)/3600;
+
+	double Sm = S.Mag();
+	if ( last_Sm == 0 && Sm != 0 )
+	{	// startup
+		total_cost += startup_cost * operating_capacity;
+	}
+	else if ( last_Sm != 0 && Sm == 0 )
+	{	// shutdown
+		total_cost += shutdown_cost * operating_capacity;
+	}
+	double energy = last_Sm * dt;
+	total_cost += fixed_cost*dt + (variable_cost + scarcity_cost*energy) * energy;
+
+	extern bool with_emissions;
+	if ( with_emissions )
+	{
+		total_emissions = emissions_rate * energy * dt;
+	}
+
+	last_t = t0;
+	last_Sm = Sm;
+
 	return TS_NEVER;
 }
