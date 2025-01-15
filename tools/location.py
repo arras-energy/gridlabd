994fab419bfe999d90d676a92f50de55a769f3dc
David P. Chassin <david.chassin@me.com>
2025-01-15 05:44:13 -0800

    Create location.py

---
+++ tools/location.py
@@ -0,0 +1,67 @@
+"""Location tool
+
+Syntax: gridlabd location [OPTIONS ...]
+
+Options:
+
+* `--debug`: enable debug traceback on exception
+
+* `--quiet`: suppress error messages
+
+* `--silent`: suppress all error messages
+
+* `--warning`: suppress warning messages
+
+* `--verbose`: enable verbose output, if any
+
+"""
+
+import sys
+import framework as app
+
+def main(argv):
+
+    if len(argv) == 1:
+
+        print("\n".join([x for x in __doc__.split("\n") if x.startswith("Syntax: ")]))
+        return app.E_SYNTAX
+
+    args = app.read_stdargs(argv)
+
+    for key,value in args:
+
+        if key in ["-h","--help","help"]:
+            print(__doc__,file=sys.stdout)
+        else:
+            error(f"'{key}={value}' is invalid")
+            return app.E_INVALID
+
+    return app.E_OK
+
+if __name__ == "__main__":
+
+    try:
+
+        # TODO: development testing -- delete when done writing code
+        if not sys.argv[0]:
+            sys.argv = ["selftest","--debug"]
+
+        rc = main(sys.argv)
+        exit(rc)
+
+    except KeyboardInterrupt:
+
+        exit(app.E_INTERRUPT)
+
+    except Exception as exc:
+
+        if DEBUG:
+            raise exc
+
+        if not QUIET:
+            e_type,e_value,e_trace = sys.exc_info()
+            tb = traceback.TracebackException(e_type,e_value,e_trace).stack[1]
+            print(f"EXCEPTION [{app.EXEFILE}@{tb.lineno}]: ({e_type.__name__}) {e_value}",file=sys.stderr)
+
+        exit(app.E_EXCEPTION)
+
