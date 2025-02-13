6867fa8e7fd97d2b717886b72768a011ce0db503
David P. Chassin <david.chassin@me.com>
2025-02-13 09:06:08 -0800

    Attempt to fix makemd failure in dockerhub build
    Signed-off-by: David P. Chassin <david.chassin@me.com>

--- docs/Makefile.mk
+++ docs/Makefile.mk
@@ -6,9 +6,9 @@ DOCS_UTILITIES = $(DESTDIR)$(bindir)/gridlabd docs/makemd.py
 include $(top_srcdir)/docs/Tools/Makefile.mk
 
 docs/Tools: $(DOCS_UTILITIES)
-	echo "Updating $@..."
+	@test "$(MAKEMD)" != "no" && echo "Updating $@..." || true
 
 %.md: FORCE
-	$(DESTDIR)$(bindir)/gridlabd python docs/makemd.py $(basename $(notdir $(shell echo "$@" | tr 'A-Z' 'a-z'))) $(dir $@)
+	@test "$(MAKEMD)" != "no" && $(DESTDIR)$(bindir)/gridlabd python docs/makemd.py $(basename $(notdir $(shell echo "$@" | tr 'A-Z' 'a-z'))) $(dir $@) || true
 
 FORCE:
