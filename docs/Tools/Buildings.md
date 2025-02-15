c626afebe683b28449a9d6a2ea6599e8905cb5ef
David P. Chassin <david.chassin@me.com>
2025-02-15 11:36:08 -0800

    Add Python example
    Signed-off-by: David P. Chassin <david.chassin@me.com>

--- docs/Tools/Buildings.md
+++ docs/Tools/Buildings.md
@@ -16,6 +16,52 @@ Options:
 
 * `--cleancache`: clean cache data
 
+Description:
+
+The `buildings` module accessing the building model database used to generate building
+objects in GridLAB-D.
+
+Examples:
+
+To download buildings data from the command line
+~~~
+gridlabd buildings -C=US/NH/Sullivan
+~~~
+
+To access building data in Python
+
+~~~
+gridlabd python
+>>> import gridlabd.buildings
+>>> gridlabd.buildings.Buildings("US","NH","Sullivan").data
+                              id climate  year   centroid                                       footprint  height  ground_area  code  class  mixed  type  0      87M95X2C+XC65-13-14-14-13      6A     0  drsv2zwhg               drsv2zwhe18,bcz,j756,jjb2,ucx,uk3     5.4        119.8     5      1  False     0   
+1      87M95X3C+7H47-15-14-15-14      6A     0  drsv2zy3j                      drsv2zy2vbx,34x1,3skb,3pwq     6.6        145.8     7      3  False     0   
+2        87M95XQQ+C4QJ-11-9-11-9      6A     0  drsv9q4rg                       drsv9q4r7qb,ct2,6255f,sv7     3.8         69.0     1      1  False     0   
+3      87M95XQQ+95GJ-15-18-16-17      6A     0  drsv9q4qv  drsv9q4wb8j,qmyh,que2,qer5,r433,rm5k,qyw1,rp9u     5.3        170.4     2      3  False     0   
+4      87M95XPJ+Q24Q-12-10-12-11      6A     0  drsv9juty                       drsv9jutx11,u3q,wjz2,vbr5     5.1         95.0     5      1  False     0   
+...                          ...     ...   ...        ...                                             ...     ...          ...   ...    ...    ...   ...   
+27571  87M96HQF+7RQ9-12-16-13-17      6A  1890  drsmzzm2y                      drsmzzm353c,90vq,893d,27tv    10.5        228.0     1      1  False    91   
+27572  87M99M94+8C96-33-32-36-30      6A  1965  drsw9yby8  drsw9ybznxv,yjt4,y60g,v924,tew2,ws6h,wtk0,xw0v     7.0        914.1     1     11  False   150   
+27573    87M99M97+VXMQ-16-8-15-8      6A  1910  drsw9zpny                       drsw9zppm6h,reg,q80x,nmyz     7.2        123.0     1      1  False     7   
+27574   87M9CWVQ+MX5X-10-10-10-9      6A     0  drsz0hz88                       drsz0hz80y6,2qtc,2z9v,9fr     5.0         72.1     1      1  False     0   
+27575   87M99J6X+PM69-10-17-9-18      6A  1983  drsw9tg8x               drsw9tg87z4,guj,v7e,vrk,bfn4,b3vq     3.0        194.6     2      1  False    91   
+
+       windows  floors  floor_area  
+0        0.140       1       119.8  
+1        0.300       2       291.7  
+2        0.140       1        69.0  
+3        0.300       1       170.4  
+4        0.140       1        95.0  
+...        ...     ...         ...  
+27571    0.140       3       684.1  
+27572    0.006       1       914.1  
+27573    0.140       2       245.9  
+27574    0.140       1        72.1  
+27575    0.140       1       194.6  
+
+[27576 rows x 14 columns]
+~~~
+
 
 
 # Classes
@@ -49,115 +95,17 @@ Arguments:
 
 Buildings exception
 
----
-
-## Resource
-
-Resource class
-
-### `Resource()`
-
-Construct resource object
-
-Arguments:
-
-* `file`: resource file (default is `$GLD_ETC/resource.csv`)
-
-
-### `Resource.content() -> str`
-
-Get resource content
-
-Arguments:
-
-* `name`: resource name
-
-* `index`: resource index
-
-* `**kwargs`: options (see `properties()`)
-
-Returns:
-
-* `str`: Resource contents
-
-
-### `Resource.headers() -> Union`
-
-Get resource header
-
-* `name`: resource name
-
-* `index`: resource index
-
-* `**kwargs`: options (see `properties()`)
-
-Returns:
-
-* `str`: header content if a simple string
-
-* `list`: header content if a list
-
-* `dict`: header contents if a dict
-
-
-### `Resource.index(kwargs:dict) -> Union`
-
-Get resource index (if any)
-
-Arguments:
-
-* `kwargs`: property keys to collect
-
-Returns:
-
-* `str`: a single index value
-
-* `list`: a list of index values
-
-* `dict`: a dict of index values
-
-
-### `Resource.list(pattern:str) -> list`
-
-Get a list of available resources
-
-Arguments:
-
-* `pattern`: regular expression for resource names to be returned
-
-
-### `Resource.properties(passthru:str, kwargs:dict) -> dict`
-
-Get resource properties
-
-Arguments:
-
-* `passthru`: resource keys that are passed through if not resolved
-
-* `kwargs`: keys to include in resolving properties
-
-Returns:
-
-`dict`: resolved properties
-
-Description:
-
-The following keys are commonly found in resource properties:
-
-* `index`: the resource index
-
-* `origin`: the resource origin on github, e.g. `{organization}/{repo}`
-
-* `organization`: the github organization
-
-* `gitbranch`: the resource branch on github
-
-
 # Functions
 
-## `geocode() -> None`
+## `main() -> int`
 
+Main routine
 
-Decode geohash, returning two float with latitude and longitude
-containing only relevant digits and with trailing zeroes removed.
+Arguments:
+
+* `argv`: command line argument list (see module docs for details)
+
+Returns:
+
+* `int`: exit code
 
--- tools/buildings.py
+++ tools/buildings.py
@@ -16,10 +16,51 @@ Options:
 
 * `--cleancache`: clean cache data
 
+Description:
+
+The `buildings` module accessing the building model database used to generate building
+objects in GridLAB-D.
+
 Examples:
 
+To download buildings data from the command line
 ~~~
 gridlabd buildings -C=US/NH/Sullivan
+~~~
+
+To access building data in Python
+
+~~~
+gridlabd python
+>>> import gridlabd.buildings
+>>> gridlabd.buildings.Buildings("US","NH","Sullivan").data
+                              id climate  year   centroid                                       footprint  height  ground_area  code  class  mixed  type  \
+0      87M95X2C+XC65-13-14-14-13      6A     0  drsv2zwhg               drsv2zwhe18,bcz,j756,jjb2,ucx,uk3     5.4        119.8     5      1  False     0   
+1      87M95X3C+7H47-15-14-15-14      6A     0  drsv2zy3j                      drsv2zy2vbx,34x1,3skb,3pwq     6.6        145.8     7      3  False     0   
+2        87M95XQQ+C4QJ-11-9-11-9      6A     0  drsv9q4rg                       drsv9q4r7qb,ct2,6255f,sv7     3.8         69.0     1      1  False     0   
+3      87M95XQQ+95GJ-15-18-16-17      6A     0  drsv9q4qv  drsv9q4wb8j,qmyh,que2,qer5,r433,rm5k,qyw1,rp9u     5.3        170.4     2      3  False     0   
+4      87M95XPJ+Q24Q-12-10-12-11      6A     0  drsv9juty                       drsv9jutx11,u3q,wjz2,vbr5     5.1         95.0     5      1  False     0   
+...                          ...     ...   ...        ...                                             ...     ...          ...   ...    ...    ...   ...   
+27571  87M96HQF+7RQ9-12-16-13-17      6A  1890  drsmzzm2y                      drsmzzm353c,90vq,893d,27tv    10.5        228.0     1      1  False    91   
+27572  87M99M94+8C96-33-32-36-30      6A  1965  drsw9yby8  drsw9ybznxv,yjt4,y60g,v924,tew2,ws6h,wtk0,xw0v     7.0        914.1     1     11  False   150   
+27573    87M99M97+VXMQ-16-8-15-8      6A  1910  drsw9zpny                       drsw9zppm6h,reg,q80x,nmyz     7.2        123.0     1      1  False     7   
+27574   87M9CWVQ+MX5X-10-10-10-9      6A     0  drsz0hz88                       drsz0hz80y6,2qtc,2z9v,9fr     5.0         72.1     1      1  False     0   
+27575   87M99J6X+PM69-10-17-9-18      6A  1983  drsw9tg8x               drsw9tg87z4,guj,v7e,vrk,bfn4,b3vq     3.0        194.6     2      1  False    91   
+
+       windows  floors  floor_area  
+0        0.140       1       119.8  
+1        0.300       2       291.7  
+2        0.140       1        69.0  
+3        0.300       1       170.4  
+4        0.140       1        95.0  
+...        ...     ...         ...  
+27571    0.140       3       684.1  
+27572    0.006       1       914.1  
+27573    0.140       2       245.9  
+27574    0.140       1        72.1  
+27575    0.140       1       194.6  
+
+[27576 rows x 14 columns]
 ~~~
 """
 
@@ -29,9 +70,9 @@ import pandas as pd
 import requests
 import urllib
 import gridlabd.framework as app
-from gridlabd.resource import Resource
+import gridlabd.resource as res
 import gridlabd.geodata_address as address
-from gridlabd.nsrdb_weather import geocode
+import gridlabd.nsrdb_weather as nw
 from typing import TypeVar
 
 class BuildingsError(app.ApplicationError):
@@ -75,7 +116,7 @@ class Buildings:
 
         # read from source
         else:
-            self.data = Resource().content(name="buildings",index=f"{country}/{filename}")
+            self.data = res.Resource().content(name="buildings",index=f"{country}/{filename}")
             changed = True
 
         # handle latlon include option
@@ -93,11 +134,34 @@ class Buildings:
         # save cache
         if cache and changed:
 
+            os.makedirs(filedir,exist_ok=True)
             self.data.to_csv(pathname,index=False,header=True)
+
+    def list(self):
+        """Get data as list
+
+        Returns:
+
+        * `list`: see `pandas.DataFrame.to_dict('list')` for details
+        """
+        return self.data.to_dict('records')
+
+    def dict(self,orient='dict'):
+        """Get data as dict
+
+        Arguments:
+
+        * `orient`: see `pandas.DataFrame.to_dict()` for details
+
+        Returns:
+
+        * `dict|list`: data organized according to `orient`
+        """
+        return self.data.to_dict(orient)
 
     def _add_latlon(self):
 
-        latlon = [geocode(x) for x in self.data["centroid"].tolist()]
+        latlon = [nw.geocode(x) for x in self.data["centroid"].tolist()]
         self.data["latitude"] = [x[0] for x in latlon]
         self.data["longitude"] = [x[1] for x in latlon]
 
@@ -106,7 +170,16 @@ class Buildings:
         self.data["address"] = address.apply(self.data)
 
 def main(argv:list[str]) -> int:
+    """Main routine
 
+    Arguments:
+
+    * `argv`: command line argument list (see module docs for details)
+
+    Returns:
+
+    * `int`: exit code
+    """
     OUTPUT = None
     LOCATE = False
     ADDRESS = False
@@ -127,7 +200,7 @@ def main(argv:list[str]) -> int:
         if key in ["-h","--help","help"]:
             print(__doc__,file=sys.stdout)
 
-        elif key in ["-A","--address"]:
+        elif key in ["-A","--address"] and value is None:
 
             ADDRESS = True
 
@@ -135,7 +208,7 @@ def main(argv:list[str]) -> int:
 
             country,state,county = value[0].split("/")
 
-        elif key in ["-L","--locate"]:
+        elif key in ["-L","--locate"] and value is None:
 
             LOCATE = True
 
@@ -143,11 +216,11 @@ def main(argv:list[str]) -> int:
 
             OUTPUT = value
 
-        elif key == "--nocache":
+        elif key == "--nocache" and value is None:
 
             CACHE = False
 
-        elif key == "--cleancache":
+        elif key == "--cleancache" and value is None:
 
             CACHE = "clean"
         else:
