68ed9597b64adf7ad0a4ecba80afa47a37b4f90e
David P. Chassin <david.chassin@me.com>
2025-02-21 14:36:11 -0800

    Update arras deployment (#290)
    Signed-off-by: Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
    Co-authored-by: Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>

--- install/ubuntu_24.sh
+++ install/ubuntu_24.sh
@@ -1,10 +1,10 @@
-apt update 
+apt update
 apt install curl -y
 . /etc/os-release
 
-LATEST=$(curl -sL http://install.gridlabd.us/master.txt)
+LATEST=$(curl -sL http://install.arras.energy/master.txt)
 
-curl -sL https://install.gridlabd.us/
+curl -sL https://install.arras.energy/
 
 apt-get install git -y
 git clone https://github.com/$GRIDLABD_ORG/$GRIDLABD_REPO -b $GRIDLABD_BRANCH --depth 1 gridlabd
