fa96d688d29a601a50db6af9aadea06798cb3ae5
David P. Chassin <david.chassin@me.com>
2025-02-21 10:12:58 -0800

    Update noaa_forecast autotest

--- tools/noaa_forecast.py
+++ tools/noaa_forecast.py
@@ -144,7 +144,7 @@ def getforecast(lat,lon):
         result["temperature[degF]"].append(float(item["temperature"]))
         result["wind_speed[m/s]"].append(float(item["windSpeed"].split()[0])*0.44704)
         wind_dirs = dict(
-            N=0, NNE=22.5, NE=45, ENE=67.5, E=90, ESE=112.5, SE=135, SSE=157.5,
+            N=0.0, NNE=22.5, NE=45, ENE=67.5, E=90, ESE=112.5, SE=135, SSE=157.5,
             S=180, SSW=202.5, SW=225, WSW=247.5, W=270, WNW=292.5, NW=315, NNW=337.5,
             )
         wind_dir = item["windDirection"]
