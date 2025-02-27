48e6818528a91a2f6f86ca8f5964897bf6ffbd72
David P. Chassin <david.chassin@me.com>
2025-02-27 14:25:22 -0800

    Verify OPF testcase results

--- module/pypower/autotest/verify.py
+++ module/pypower/autotest/verify.py
@@ -3,19 +3,19 @@ def write_glm(glm,case):
     with open(glm,"w") as fh:
         for bus in case['bus']:
             print(f"""object assert
-    {{      
-        parent pp_bus_{bus[0]:.0f};
-        target Vm;
-        relation ==;
-        value {bus[7]:.3f};
-        within ${{MRES}};
-    }}
-    object assert
-    {{
-        parent pp_bus_{bus[0]:.0f};
-        target Va;
-        relation ==;
-        value {bus[8]:.2f};
-        within ${{ARES}};
-    }}
-    """,file=fh)
+{{      
+    parent pp_bus_{bus[0]:.0f};
+    target Vm;
+    relation ==;
+    value {bus[7]:.3f};
+    within ${{MRES}};
+}}
+object assert
+{{
+    parent pp_bus_{bus[0]:.0f};
+    target Va;
+    relation ==;
+    value {bus[8]:.2f};
+    within ${{ARES}};
+}}
+""",file=fh)
