9853aabbeca363e8cc824d20d7c2462b1ef282a2
Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>
2025-06-19 17:07:20 -0700

    adjusted syntax to new tested shell commands for install.sh
    Signed-off-by: Duncan Ragsdale <88173870+Thistleman@users.noreply.github.com>

--- cloud/websites/install.arras.energy/install.sh
+++ cloud/websites/install.arras.energy/install.sh
@@ -51,12 +51,13 @@ else
 		else
 			if [ ! -e "$GRIDLABD_FOLDER/bin/python3" ] ; then
 				basepython=$(find "$GRIDLABD_FOLDER/bin" -name "python3.*" )
+				basepython=$(basename $basepython )
 					if ! [ -x $(which $basepython) ]; then
 						echo "Error: $basepython is not installed." >${INSTALL_STDERR:-$DEFAULT_STDERR}
 						echo "You will need to install the correct python version an update the symlinks for gridlabd to work correctly." >${INSTALL_STDERR:-$DEFAULT_STDERR}
 					else
 						rm -rf "$GRIDLABD_FOLDER/bin/python*"
-						basepython -m venv $GRIDLABD_FOLDER
+						eval $basepython -m venv $GRIDLABD_FOLDER
 					fi
 			fi
 			ln -sf "$GRIDLABD_FOLDER" "current" 1>${INSTALL_STDOUT:-$DEFAULT_STDOUT} 2>${INSTALL_STDERR:-$DEFAULT_STDERR}
