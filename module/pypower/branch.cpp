41edd705b0172a55d7b8412307fe1397c78651fa
David P. Chassin <david.chassin@me.com>
2025-07-23 15:41:29 -0700

    Update branch.cpp

--- module/pypower/branch.cpp
+++ module/pypower/branch.cpp
@@ -126,81 +126,243 @@ int branch::create(void)
 	set_current(0.0);
 	set_loss(0.0);
 
+	fromKV = 0.0;
+	toKV = 0.0;
+	length = 0.0;
+
 	return 1; /* return 1 on success, 0 on failure */
 }
 
 int branch::init(OBJECT *parent)
 {
-	if ( get_from() == NULL || get_to() == NULL )
+	// check from bus
+	if ( get_from() == NULL )
 	{
-		error("from or to bus not specified");
+		error("from bus not specified");
 		return 0;
 	}
+	bus *f = OBJECTDATA(get_from(),bus);
+	if ( ! f->isa("bus","pypower") )
+	{
+		error("from object '%s' is not a pypower bus",f->get_name());
+		return 0;
+	}
+	fobj = get_object(get_from());
+
+	// check to bus
+	if ( get_to() == NULL )
+	{
+		error("to bus not specified");
+		return 0;
+	}
+	bus *t = OBJECTDATA(get_to(),bus);
+	if ( ! t->isa("bus","pypower") )
+	{
+		error("to object '%s' is not a pypower bus",t->get_name());
+		return 0;
+	}
+	tobj = get_object(get_to());
+
 	// automatic bus lookup
 	if ( get_fbus() == 0 )
 	{
-		bus *f = OBJECTDATA(get_from(),bus);
-		if ( f == NULL )
+		if ( f->get_bus_i() == 0 )
 		{
-			error("cannot determine 'fbus' if 'from' is not specified");
-			return 0;
+			return 2; // defer until bus is initialized
 		}
-		if ( f->isa("bus","pypower") )
-		{
-			if ( f->get_bus_i() == 0 )
-			{
-				return 2; // defer until bus is initialized
-			}
-			set_fbus(f->get_bus_i());
-		}
-		else
-		{
-			error("from object '%s' is not a pypower bus",f->get_name());
-			return 0;
-		}
+		set_fbus(f->get_bus_i());
 	}
 	if ( get_tbus() == 0 )
 	{
-		bus *t = OBJECTDATA(get_to(),bus);
-		if ( t == NULL )
+		if ( t->get_bus_i() == 0 )
 		{
-			error("cannot determine 'tbus' if 'to' is not specified");
-			return 0;
+			return 2; // defer until bus is initialized
 		}
-		if ( t->isa("bus","pypower") )
-		{
-			if ( t->get_bus_i() == 0 )
-			{
-				return 2; // defer until bus is initialized
-			}
-			set_tbus(t->get_bus_i());
-		}
-		else
-		{
-			error("from object '%s' is not a pypower bus",t->get_name());
-			return 0;
-		}
+		set_tbus(t->get_bus_i());
 	}
-	if ( ratio == 0 )
+
+	if ( rateA <= 0 )
 	{
-		ratio = 1.0;
-	}
-	if ( x == 0 )
-	{
-		warning("x is zero");
-	}
-	if ( rateA == 0 )
-	{
-		warning("rateA is zero");
+		error("rateA is not specified");
+		return 0;
 	}
 	if ( rateB > 0 && rateB < rateA )
 	{
-		warning("rateB is less than rateA");
+		warning("rateB is less than rateA, setting rateB equal to rateA");
 	}
+	rateB = max(rateA,rateB);
 	if ( rateC > 0 && rateC < rateB )
 	{
-		warning("rateC is less than rateB");
+		warning("rateC is less than rateB, setting rateC equal to rateB");
 	}
+	rateC = max(rateB,rateC);
+	
+	fromKV = f->get_baseKV();
+	toKV = t->get_baseKV();
+	is_transformer = ( ratio != 0 );
+
+	// haversine calculation
+	length = fobj->get_distance(tobj);
+	if ( ratio == 0 && ( r == 0 || x == 0 ) )
+	{
+		if ( isnan(length) )
+		{
+			warning("unable to compute default line r/x/b without lat/lon for from/to bus");
+		}
+		else
+		{
+			verbose("distance from %.6lf,%.6lf to %.6lf,%.6lf is %.1lf miles\n",
+				fobj->get_latitude(),fobj->get_longitude(),
+				tobj->get_latitude(),tobj->get_longitude(),
+				length);
+		}
+	}
+	is_device = ( ratio == 0 && length == 0 );
+
+	// // per-unit impedances
+	extern double base_MVA;
+	frompuZ = fromKV*fromKV/base_MVA;
+	topuZ = toKV*toKV/base_MVA;
+	
+	// Generally realistic lines parameters by voltage level
+
+		// 138 kv lines
+		//   r = 0.1 - 0.25 ohms/mile
+		//   x = 0.60 ohms/mile
+		//   b = 4.5 micro-mhos/mile
+
+		// 230 kv lines
+		//   r = 0.05 - 0.1 ohms/mile
+		//   x = 0.50 ohms/mile
+		//   b = 5.5 micro-mhos/mile
+
+		// 345 kv lines
+		//   r = 0.03 - 0.05 ohms/mile
+		//   x = 0.40 ohms/mile
+		//   b = 6.0 micro-mhos/mile
+
+		// 500 kv lines
+		//   r = 0.02 - 0.04 ohms/mile
+		//   x = 0.30 ohms/mile
+		//   b = 6.5 micro-mhos/mile
+
+		// Linear fit for line parameters (using best-in-range values):
+		//   r = 0.0052*fromKV+4.0373 Ohms/mile
+		//   x = -0.0008*fromKV+0.699 Ohms/mile
+		//   b = -0.0002*fromKV+0.1122 uS/mile
+
+		// Source: https://www.eng-tips.com/threads/typical-transmission-line-parameters.65375/
+
+	// Generally accepted transformer parameters by rating
+
+		// Dry-type 3-phase:
+		//  <200 kVA   		r = 1.5 - 6.0 %
+		//  200 - 500 kVA   r = 3.0 - 7.0 %
+		//  500 - 750 kVA   r = 4.5 - 8.0 %
+		//  >750 kVA        r = 5.0 - 8.0 %
+
+		// Liquid-type 3-phase:
+		//  <75 kVA         r = 1.0 - 4.5 %
+		//	75 - 100 kVA	r = 1.0 - 5.0 %
+		//	100 - 400 kVA	r = 1.2 - 6.0 %
+		//	400 - 750 KVA	r = 1.5 - 7.0 %
+		//	>750 kVA		r = 5.0 - 7.5 %	
+
+		// Log fit for r:
+		//	r = 0.03048*log(fromKV)-0.04209
+
+		// Source: https://energycodeace.com/site/custom/public/reference-ace-t20/index.html#!Documents/gloss_specialimpedancetransformer.htm
+
+		// X/R ratios
+		// rating 	x/r  
+		// 5		12
+		// 10		15
+		// 20		20
+		// 50		30
+		// 100		40
+		// 200		50
+		// 500		65
+
+		// Log fit for x/r:
+		//	x/r = max(5,26.8608*log(rating_MVA)-11.7491)
+	
+		// Source:        IEEE Std C37.010-2016
+
+		//   b = 1/(r+jx)
+
+	// Generally accepted device parameters by voltage level
+
+		// r = 0.001 Ohm
+		// x = 0.010 Ohm
+		// b = 1/(r+jx)
+
+	if ( r == 0 && frompuZ > 0 )
+	{
+		if ( is_device )
+		{
+			// device, e.g., contactor
+			r = 0.001/frompuZ; // 1 mOhm (?)
+			warning("device r is zero, setting to default %.03lg pu.mOhm",r*1e3);
+		}
+		else if ( is_transformer )
+		{
+			// transformer
+			r = (0.0348*log(fromKV)-0.04209)/frompuZ;
+			warning("transformer r is zero, setting to default %.4lg pu.Ohm for %.1lf kV",r,fromKV);
+
+		}
+		else
+		{
+			// power line
+			r = (0.0052*fromKV+4.0373)*length/frompuZ;
+			warning("line r is zero (defaulting to %.4lg Ohms for %.3lg kV %.1lf mile line)",r,fromKV,length);
+		}
+	}
+	
+	if ( x == 0 && frompuZ > 0 )
+	{
+		if ( is_device )
+		{
+			// device, e.g., contactor
+			x = 10*r; // x/r = 10 (?)
+			warning("device x is zero, setting to default %.03lg pu.mOhm",x*1e3);
+		}
+		else if ( is_transformer )
+		{
+			// transformer
+			x = r*max(26.8608*log(rateA)-11.7491,5.0);
+			warning("transformer x is zero, setting to default %.4lg pu.Ohm for %.1lf MVA",x,rateA);
+		}
+		else
+		{
+			// power line
+			x = (-0.0008*fromKV+0.699)*length/frompuZ;
+			warning("line x is zero (defaulting to %.4lg Ohms for %.3lg kV %.1lf mile line)",x,fromKV,length);
+		}
+	}
+
+	if ( b == 0 && frompuZ > 0 )
+	{
+		if ( is_device )
+		{
+			// device, e.g., contactor
+			b = -x/(x*x+r*r);
+			warning("device b is zero, setting to default %.3lf pu.S for z=%.4lg%+.4lgj pu.Ohm",b,r,x);
+		}
+		else if ( is_transformer )
+		{
+			// transformer
+			b = -x/(x*x+r*r);
+			warning("transformer b is zero, setting to default %.1lf pu.S for z=%.4lg%+.4lgj pu.Ohm",b*1e6,r,x);
+		}
+		else
+		{
+			// power line
+			b = (-0.0002*fromKV+0.1122)*1e-6*length*frompuZ;
+			warning("b is zero (defaulting to %.4lg pu.S for %.3lg kV %.1lf mile line)",b,fromKV,length);
+		}
+	}	
+
 	if ( angmin == 0 )
 	{
 		angmin = -360;
@@ -209,5 +371,6 @@ int branch::init(OBJECT *parent)
 	{
 		angmax = +360;
 	}
+	
 	return 1;
 }
