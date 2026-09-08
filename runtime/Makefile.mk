a12c96bd46e6cf3f50daa469b630ea267b2c673b
David P. Chassin <dchassin@eudoxys.com>
2026-09-08 09:07:39 -0700

    Move pandas to runtime

--- runtime/Makefile.mk
+++ runtime/Makefile.mk
@@ -48,6 +48,7 @@ dist_pkgdata_DATA += runtime/triplex_meter_r.png
 dist_pkgdata_DATA += runtime/tzinfo.txt
 dist_pkgdata_DATA += runtime/unitfile.txt
 
-dist_pkgdata_DATA += runtime/gld_types.py
-dist_pkgdata_DATA += runtime/gld_timestamp.py
-dist_pkgdata_DATA += runtime/gld_tzinfo.py
+dist_pkgdata_DATA += runtime/pandas.glm
+dist_pkgdata_DATA += runtime/pandas.py
+# dist_pkgdata_DATA += runtime/gld_timestamp.py
+# dist_pkgdata_DATA += runtime/gld_tzinfo.py
